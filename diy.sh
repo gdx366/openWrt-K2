6972ddefc9d5711d559149f0894a5d9e91f4666c
gdx366 <531095815@qq.com>
2020-02-23 19:51:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/Lienol/openwrt-package package/lienol
+
+# Add luci-app-vssr
+git clone https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/Leo-Jo-My/my package/my  #依赖包
