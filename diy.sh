ef4e087b306b59589797d249c388c21da8abba83
gdx366 <531095815@qq.com>
2020-02-24 08:19:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,8 +10,8 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add luci-app-vssr
-#git clone https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-vssr
-#git clone https://github.com/Leo-Jo-My/my package/my
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
-#./scripts/feeds update -a
-#./scripts/feeds install -a
+./scripts/feeds update -a
+./scripts/feeds install -a
