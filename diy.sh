af9b52c2f0392b0b9f02975b8c69a46e37ab2b94
gdx366 <531095815@qq.com>
2020-02-28 14:22:25 +0800

    修改配置为什么插件都不加

--- diy.sh
+++ diy.sh
@@ -10,8 +10,8 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add luci-app-vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
