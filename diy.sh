3e9b2f3faa7cf2dd0f99d021f8adb3d23c2e1c89
gdx <531095815@qq.com>
2020-04-10 18:30:28 +0800

    编译N1 添加固件 vssr+passwall

--- diy.sh
+++ diy.sh
@@ -8,15 +8,16 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
 # Add appfilter
-git clone https://github.com/destan19/OpenAppFilter/tree/master/luci-app-oaf.git package/luci-app-oaf
-git clone https://github.com/destan19/OpenAppFilter/tree/master/oaf.git package/oaf
-git clone https://github.com/destan19/OpenAppFilter/tree/master/open-app-filter.git package/open-app-filter
+#git clone https://github.com/destan19/OpenAppFilter/tree/master/luci-app-oaf.git package/luci-app-oaf
+#git clone https://github.com/destan19/OpenAppFilter/tree/master/oaf.git package/oaf
+#git clone https://github.com/destan19/OpenAppFilter/tree/master/open-app-filter.git package/open-app-filter
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
